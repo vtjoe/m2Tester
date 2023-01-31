b8bafd42738b5bbae21d90377a73efc7082c8fe1
Joe Breuer <joebreuer@gmail.com>
2023-01-31 10:11:18 -0500

    Tweaked monitoring script and Done duration

--- monitorDataStrip.sh
+++ monitorDataStrip.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-date
-FILE="FRCDISP1.TXT"
-if [[ ! -f $FILE ]]
-then
-    echo "$FILE does not exist"
-    exit
+result=`ps aux | grep -i "strip-m2TestData.sh" | grep -v "grep" | wc -l`
+if [ $result -eq 0 ]
+   then
+        /home/pi/m2Tester/strip-m2TestData.sh > /dev/null 2>&1 & 
+        echo "starting script "
+   else
+        echo "script is running"
+	ps aux | grep -i "strip-m2TestData.sh" | grep -v "grep" | wc -l`
+	exit
 fi
-countSource=`cat $FILE | wc -l`
-echo $countSource
-
-filename=`ls -dt read* | head -1`
-echo $filename
-countTarget=`cat $filename | wc -l`
-echo $countTarget
-
-for ((i=1;i<=100;i++))
-do  
-#clear
-printf  "Source =$countSource Target =$countTarget counter =$i \n "
-sleep 10
-countTarget=`cat $filename | wc -l`
-done
-
-date
-
