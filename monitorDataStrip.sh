2af30ad9686c9dda6dbfaea85802aae4fa75c165
Joe Breuer <joebreuer@gmail.com>
2023-01-31 15:13:20 -0500

    Found matrix messages missing quote

--- monitorDataStrip.sh
+++ monitorDataStrip.sh
@@ -3,9 +3,10 @@ result=`ps aux | grep -i "strip-m2TestData.sh" | grep -v "grep" | wc -l`
 if [ $result -eq 0 ]
    then
         /home/pi/m2Tester/strip-m2TestData.sh > /dev/null 2>&1 & 
+	echo " Result ==> $result "
         echo "starting script "
    else
         echo "script is running"
-	ps aux | grep -i "strip-m2TestData.sh" | grep -v "grep" | wc -l`
+	echo " Result else ==> $result "
 	exit
 fi
